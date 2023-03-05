1096129db6db00d97800fc69439cced0bae3fb03
Yoren Schriever <yoren@ocelot.nl>
2023-03-05 18:16:56 +0100

    cleanup

--- projects/tetra/main.cpp
+++ projects/tetra/main.cpp
@@ -1,38 +1,30 @@
+#include "ChaserLedAnimation.h"
+#include "LedShape.h"
+#include "animationPattern.h"
 #include "colours.h"
 #include "core/distribution/inputs/inputSplitter.hpp"
 #include "core/distribution/inputs/patternInput.hpp"
 #include "core/distribution/inputs/udpInput.hpp"
 #include "core/distribution/outputs/monitorOutput.hpp"
 #include "core/distribution/outputs/neopixelOutput.hpp"
-#include "core/distribution/outputs/nullOutput.hpp"
-#include "core/distribution/outputs/udpOutput.hpp"
 #include "core/distribution/pipes/convertPipe.hpp"
 #include "core/generation/patterns/mappedPatterns.h"
 #include "core/generation/pixelMap.hpp"
 #include "core/generation/pixelMapSplitter.hpp"
 #include "core/hyperion.hpp"
-#include "platform/includes/ethernet.hpp"
-#include "platform/includes/ipAddress.hpp"
-#include "platform/includes/log.hpp"
-#include "platform/includes/socket.hpp"
-#include "platform/includes/thread.hpp"
-#include <vector>
-#include "animationPattern.h"
-#include "LedShape.h"
-#include "ChaserLedAnimation.h"
 
 #include "ChaserLedAnimation.cpp"
 
 void addLedShapes(Hyperion *hyp);
 
-  Installation tetra = {
-    LedShape(12 , 3), //  25cm Triangle ( 12 LEDs per side,  36 total,  2.16A powerdraw)
-    LedShape(33 , 3), //  60cm Triangle ( 33 LEDs per side,  99 total,  5.94A powerdraw)
-    LedShape(54 , 3), //  95cm Triangle ( 54 LEDs per side, 162 total,  9.72A powerdraw)
-    LedShape(75 , 3), // 130cm Triangle ( 75 LEDs per side, 225 total, 13.50A powerdraw)
-    LedShape(96 , 3), // 165cm Triangle ( 96 LEDs per side, 288 total, 17.28A powerdraw)
+Installation tetra = {
+    LedShape(12, 3),  //  25cm Triangle ( 12 LEDs per side,  36 total,  2.16A powerdraw)
+    LedShape(33, 3),  //  60cm Triangle ( 33 LEDs per side,  99 total,  5.94A powerdraw)
+    LedShape(54, 3),  //  95cm Triangle ( 54 LEDs per side, 162 total,  9.72A powerdraw)
+    LedShape(75, 3),  // 130cm Triangle ( 75 LEDs per side, 225 total, 13.50A powerdraw)
+    LedShape(96, 3),  // 165cm Triangle ( 96 LEDs per side, 288 total, 17.28A powerdraw)
     LedShape(117, 3), // 200cm Triangle (117 LEDs per side, 351 total, 21.06A powerdraw)
-  };
+};
 
 int main()
 {
@@ -53,17 +45,18 @@ void addLedShapes(Hyperion *hyp)
   // the generation of the Installation and the PixelMap can be combined
   const float pixelPitch = 0.014;
   PixelMap tetraMap;
-  for (LedShape shape: tetra){
-    for(int set = 0; set<shape.numSets; set++){
-      float x = ((shape.numLedsPerSet+1) * pixelPitch)/-2.;
+  for (LedShape shape : tetra)
+  {
+    for (int set = 0; set < shape.numSets; set++)
+    {
+      float x = ((shape.numLedsPerSet + 1) * pixelPitch) / -2.;
       float y = x * 0.59;
-      float a = float(set)/3. * 2 * M_PI;
-      for(int led = 0; led<shape.numLedsPerSet; led++){
+      float a = float(set) / 3. * 2 * M_PI;
+      for (int led = 0; led < shape.numLedsPerSet; led++)
+      {
         x += pixelPitch;
-        tetraMap.push_back({
-          .x = x * cos(a) + y * sin(a),
-          .y = y * cos(a) - x * sin(a)
-        });
+        tetraMap.push_back({.x = x * cos(a) + y * sin(a),
+                            .y = y * cos(a) - x * sin(a)});
       }
     }
   }
@@ -74,8 +67,8 @@ void addLedShapes(Hyperion *hyp)
   auto splitInput = new InputSplitter(
       new PatternInput<RGBA>(
           tetraMap.size(),
-          //new Mapped::ConcentricWavePattern<SinFast>(tetraMap, 2, 2)),
-          new AnimationPattern(&tetra, new ChaserLedAnimation(tetra,ChaserLedAnimation::relativeSize / 3, true))),
+          // new Mapped::ConcentricWavePattern<SinFast>(tetraMap, 2, 2)),
+          new AnimationPattern(&tetra, new ChaserLedAnimation(tetra, ChaserLedAnimation::relativeSize / 3, true))),
       {12 * 3 * sizeof(RGBA),
        33 * 3 * sizeof(RGBA),
        54 * 3 * sizeof(RGBA),
@@ -85,7 +78,7 @@ void addLedShapes(Hyperion *hyp)
       true);
 
   auto splitMap = PixelMapSplitter(
-      &tetraMap, {12*3,33*3,54*3,75*3,96*3,117*3});
+      &tetraMap, {12 * 3, 33 * 3, 54 * 3, 75 * 3, 96 * 3, 117 * 3});
 
   for (int i = 0; i < splitInput->size(); i++)
   {
