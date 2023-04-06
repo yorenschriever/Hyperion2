0d790b7deb95230e1790591f73f5401ff783a524
Yoren Schriever <yoren@ocelot.nl>
2023-04-06 18:27:07 +0200

    extra palettes

--- projects/wings/palettes.hpp
+++ projects/wings/palettes.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include "colours.h"
-#include "gradient.hpp"
 #include "generation/controlHub/paletteColumn.hpp"
+#include "gradient.hpp"
 
-RGB darken(RGB col, uint8_t dim){
+//https://webgradients.com/
+//https://color.adobe.com/create/color-wheel
+
+RGB darken(RGB col, uint8_t dim)
+{
     RGB result = col;
     result.dim(dim);
     return result;
@@ -81,133 +85,189 @@ Gradient sunset8 = Gradient({
 
 PaletteColumn::Palette tunnel{
     .gradient = new Gradient({
-        {.position = 0, .color = RGB(0x03A688)},  
-        {.position = 63, .color = RGB(0x026873)}, 
+        {.position = 0, .color = RGB(0x03A688)},
+        {.position = 63, .color = RGB(0x026873)},
         {.position = 127, .color = RGB(0x011F26)},
         {.position = 255, .color = RGB(0x025E73)},
 
     }),
     .primary = RGB(0x03A688),
     .secondary = RGB(0xF2668B),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette campfire{
     .gradient = new Gradient({
-        {.position = 0, .color = RGB(0x014040)},  
-        {.position = 63, .color = RGB(0x02735E)}, 
+        {.position = 0, .color = RGB(0x014040)},
+        {.position = 63, .color = RGB(0x02735E)},
         {.position = 127, .color = RGB(0x03A678)},
         {.position = 255, .color = RGB(0xF27405)},
 
     }),
     .primary = RGB(0x014040),
     .secondary = RGB(0x731702),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette greatBarrierReef{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x023059)},  
-        {.position = 63,  .color = RGB(0x0487D9)}, 
+        {.position = 0, .color = RGB(0x023059)},
+        {.position = 63, .color = RGB(0x0487D9)},
         {.position = 127, .color = RGB(0x049DD9)},
         {.position = 255, .color = RGB(0x04B2D9)},
 
     }),
     .primary = RGB(0x023059),
     .secondary = RGB(0x04D9D9),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette candy{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0xD90452)},  
-        {.position = 63,  .color = RGB(0xF205B3)}, 
+        {.position = 0, .color = RGB(0xD90452)},
+        {.position = 63, .color = RGB(0xF205B3)},
         {.position = 127, .color = RGB(0xF2AE30)},
         {.position = 255, .color = RGB(0xF28D35)},
 
     }),
-    .primary =   RGB(0xD90452),
+    .primary = RGB(0xD90452),
     .secondary = RGB(0xF26835),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette retro{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x662400)},  
-        {.position = 63,  .color = RGB(0xB33F00)}, 
+        {.position = 0, .color = RGB(0x662400)},
+        {.position = 63, .color = RGB(0xB33F00)},
         {.position = 200, .color = RGB(0xFF6B1A)},
         {.position = 255, .color = RGB(0x006663)},
 
     }),
-    .primary =   RGB(0x662400),
+    .primary = RGB(0x662400),
     .secondary = RGB(0x00B3AD),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette coralTeal{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x3F7C85)},  
-        {.position = 63,  .color = RGB(0x00CCBF)}, 
+        {.position = 0, .color = RGB(0x3F7C85)},
+        {.position = 63, .color = RGB(0x00CCBF)},
         {.position = 200, .color = RGB(0x72F2EB)},
         {.position = 255, .color = RGB(0x747E7E)},
 
     }),
-    .primary =   RGB(0x3F7C85),
+    .primary = RGB(0x3F7C85),
     .secondary = RGB(0xFF5F5D),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette pinkSunset{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x6B1B5A)},  
-        {.position = 63,  .color = RGB(0xB90852)}, 
+        {.position = 0, .color = RGB(0x6B1B5A)},
+        {.position = 63, .color = RGB(0xB90852)},
         {.position = 200, .color = RGB(0xE7083F)},
         {.position = 255, .color = RGB(0xF62D2A)},
 
     }),
-    .primary =   RGB(0xB90852),
+    .primary = RGB(0xB90852),
     .secondary = RGB(0xF7AF02),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette salmonOnIce{
     .gradient = new Gradient({
-        {.position = 0,   .color = darken(RGB(0x3F464D),100)}, 
-        {.position = 100, .color = darken(RGB(0x2186C4),150)},  
-        {.position = 180, .color = darken(RGB(0x2186C4),250)}, 
-        {.position = 190, .color = darken(RGB(0xFFF6E6),230)},  
-        {.position = 200, .color = darken(RGB(0x2186C4),50)}, 
+        {.position = 0, .color = darken(RGB(0x3F464D), 100)},
+        {.position = 100, .color = darken(RGB(0x2186C4), 150)},
+        {.position = 180, .color = darken(RGB(0x2186C4), 250)},
+        {.position = 190, .color = darken(RGB(0xFFF6E6), 230)},
+        {.position = 200, .color = darken(RGB(0x2186C4), 50)},
         {.position = 250, .color = RGB(0x7ECEFC)},
         {.position = 255, .color = RGB(0xFFF6E6)},
 
     }),
-    .primary =   RGB(0x3F464D),
+    .primary = RGB(0x3F464D),
     .secondary = RGB(0xFF8066),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette blueOrange{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x0249DA)},  
-        {.position = 63,  .color = RGB(0x0387D7)}, 
+        {.position = 0, .color = RGB(0x0249DA)},
+        {.position = 63, .color = RGB(0x0387D7)},
         {.position = 100, .color = RGB(0x039CC2)},
         {.position = 255, .color = RGB(0xF24603)},
 
     }),
-    .primary =   RGB(0x0387D7),
+    .primary = RGB(0x0387D7),
     .secondary = RGB(0xF47502),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
 
 PaletteColumn::Palette purpleGreen{
     .gradient = new Gradient({
-        {.position = 0,   .color = RGB(0x720EBF)},  
-        {.position = 63,  .color = RGB(0xff0950)}, 
-        {.position = 100, .color = darken(RGB(0xc913FF),100)},
+        {.position = 0, .color = RGB(0x720EBF)},
+        {.position = 63, .color = RGB(0xff0950)},
+        {.position = 100, .color = darken(RGB(0xc913FF), 100)},
         {.position = 200, .color = RGB(0x260580)},
         {.position = 255, .color = RGB(0x8911E5)},
     }),
-    .primary =   RGB(0xFF00FF),
+    .primary = RGB(0xFF00FF),
     .secondary = RGB(0x3EB200),
-    .highlight = RGB(255,255,255)
-};
+    .highlight = RGB(255, 255, 255)};
+
+// https://htmlcolors.com/gradient/31/beautiful-gradient
+PaletteColumn::Palette heatmap2{
+    .gradient = new Gradient({
+        {.position = 0 * 255 / 10, .color = (RGB)HSL(240 * 255 / 360, 100 * 255 / 100, 0 * 255 / 100)},
+        {.position = 1 * 255 / 10, .color = (RGB)HSL(254 * 255 / 360, 67 * 255 / 100, 13 * 255 / 100)},
+        {.position = 2 * 255 / 10, .color = (RGB)HSL(275 * 255 / 360, 82 * 255 / 100, 22 * 255 / 100)},
+        {.position = 3 * 255 / 10, .color = (RGB)HSL(297 * 255 / 360, 65 * 255 / 100, 26 * 255 / 100)},
+        {.position = 4 * 255 / 10, .color = (RGB)HSL(324 * 255 / 360, 58 * 255 / 100, 36 * 255 / 100)},
+        {.position = 5 * 255 / 10, .color = (RGB)HSL(345 * 255 / 360, 54 * 255 / 100, 47 * 255 / 100)},
+        {.position = 6 * 255 / 10, .color = (RGB)HSL(8 * 255 / 360, 70 * 255 / 100, 54 * 255 / 100)},
+        {.position = 7 * 255 / 10, .color = (RGB)HSL(25 * 255 / 360, 89 * 255 / 100, 52 * 255 / 100)},
+        {.position = 8 * 255 / 10, .color = (RGB)HSL(38 * 255 / 360, 97 * 255 / 100, 51 * 255 / 100)},
+        {.position = 9 * 255 / 10, .color = (RGB)HSL(49 * 255 / 360, 90 * 255 / 100, 61 * 255 / 100)},
+        {.position = 10 * 255 / 10, .color = (RGB)HSL(49 * 255 / 360, 90 * 255 / 100, 61 * 255 / 100)},
+
+    }),
+    .primary = (RGB)HSL(345 * 255 / 360, 54 * 255 / 100, 47 * 255 / 100),
+    .secondary = (RGB)HSL(49 * 255 / 360, 90 * 255 / 100, 61 * 255 / 100),
+    .highlight = RGB(255, 255, 255)};
+
+PaletteColumn::Palette peach{
+    .gradient = new Gradient({
+        {.position = 0, .color = RGB(0xfa709a)},
+        {.position = 255, .color = RGB(0xfee140)},
+    }),
+    .primary = RGB(0xfa709a),
+    .secondary = RGB(0xfee140),
+    .highlight = RGB(255, 255, 255)};
+
+PaletteColumn::Palette deepBlueOcean{
+    .gradient = new Gradient({
+        {.position = 0, .color = RGB(0x30cfd0)},
+        {.position = 255, .color = RGB(0x330867)},
+    }),
+    .primary = RGB(0x30cfd0),
+    .secondary = RGB(0x330867),
+    .highlight = RGB(255, 255, 255)};
+
+PaletteColumn::Palette redSalvation{
+    .gradient = new Gradient({
+        {.position = 0, .color = RGB(0xf43b47)},
+        {.position = 255, .color = RGB(0x453a94)},
+    }),
+    .primary = RGB(0xf43b47),
+    .secondary = RGB(0x453a94),
+    .highlight = RGB(255, 255, 255)};
+
+PaletteColumn::Palette denseWater{
+    .gradient = new Gradient({
+        {.position = 0 * 255 / 100, .color = RGB(0x3ab5b0)},
+        {.position = 31 * 255 / 100, .color = RGB(0x3d99be)},
+        {.position = 100 * 255 / 100, .color = RGB(0x56317a)},
+    }),
+    .primary = RGB(0x3ab5b0),
+    .secondary = RGB(0xF27811),
+    .highlight = RGB(255, 255, 255)};
+
+PaletteColumn::Palette plumBath{
+    .gradient = new Gradient({
+        {.position = 0, .color = RGB(0xcc208e)},
+        {.position = 255, .color = RGB(0x6713d2)},
+    }),
+    .primary =   RGB(0x6713d2),
+    .secondary = RGB(0x0CC2CC),
+    .highlight = RGB(255, 255, 255)};
