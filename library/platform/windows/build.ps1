c8a14094b5709c5b80c4cf395d8151d978b23c03
lennard lobrij <lennardlobrij@gmail.com>
2023-04-24 16:07:10 +0200

    added Windows build script

---
+++ library/platform/windows/build.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]$Project
+)
+# run this from the project folder root
+$HYPERION_LIB_DIR = "$($PWD.Path)\library"
+$projectDir = "$($PWD.Path)\projects\$($Project)"
+
+docker build -t hyperion $HYPERION_LIB_DIR\platform\docker
+Write-Host "Docker build complete"
+
+docker run -it --name hyperion --rm `
+    --mount type=bind,source=$HYPERION_LIB_DIR,target=/hyperion_lib `
+    --mount type=bind,source=$projectDir,target=/project `
+    --workdir /project hyperion `
+    /hyperion_lib/scripts/build.sh docker inside-container
+
+Write-Host "Docker Run complete"  
+
+docker run -it --name=hyperion --rm `
+    --mount type=bind,source=$HYPERION_LIB_DIR,target=/hyperion_lib `
+    --mount type=bind,source=$projectDir,target=/project `
+    --workdir=/project `
+    -p 443:4430 `
+    -p 9600-9800:9600-9800 `
+    hyperion `
+    /hyperion_lib/scripts/run.sh docker inside-container
