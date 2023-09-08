c584fbc680a7912a75f2cf28689b2dc050f21a6f
Yoren Schriever <yoren@ocelot.nl>
2023-09-08 11:15:27 +0200

    pharmacia project

---
+++ projects/pharmacia/main.cpp
@@ -0,0 +1,35 @@
+#include "core/distribution/inputs/inputSplitter.hpp"
+#include "core/distribution/luts/colourCorrectionLut.hpp"
+#include "core/distribution/outputs/neopixelOutput.hpp"
+#include "core/distribution/outputs/spiOutput.hpp"
+#include "core/hyperion.hpp"
+#include "distribution/inputs/patternInput.hpp"
+#include "mapping/crossMap.hpp"
+#include "palettes.hpp"
+#include "patterns.hpp"
+#include <vector>
+
+int main()
+{
+    auto hyp = new Hyperion();
+
+    Tempo::AddSource(new ConstantTempo(120));
+
+    auto input = new PatternInput<RGBA>(
+        crossMap.size(),
+        new Patterns::Cross(&crossMap));
+
+    input->params.gradient = tunnel.gradient;
+    input->params.primaryColour = tunnel.primary;
+    input->params.secondaryColour = tunnel.secondary;
+    input->params.highlightColour = tunnel.highlight;
+
+    hyp->addPipe(new ConvertPipe<RGBA, RGB>(
+        input,
+        new MonitorOutput(&hyp->webServer, &crossMap)));
+
+    hyp->start();
+
+    while (1)
+        Thread::sleep(60 * 1000);
+}
