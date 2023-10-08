82f6066917c4de154aeec39fc1e0068a1826fb7f
Yoren Schriever <yoren@ocelot.nl>
2023-10-08 15:59:06 +0200

    ledster mini

--- projects/ledster_memento/main.cpp
+++ projects/ledster_memento/main.cpp
@@ -6,9 +6,16 @@
 #include "distribution/inputs/patternCycleInput.hpp"
 #include "mapping/ledsterMap.hpp"
 #include "mapping/ledsterMap3d.hpp"
+#include "core/distribution/inputs/inputSplitter.hpp"
 #include "palettes.hpp"
 #include "patterns.hpp"
 #include <vector>
+
+// #if ESP_PLATFORM
+// #include "wifi-ap.hpp"
+// #else
+// void setup_wifi() {}
+// #endif
 
 LUT *ledsterLut = new ColourCorrectionLUT(1.5, 255, 255, 255, 240);
 
@@ -34,6 +41,12 @@ const Hyperion::Config minimal = {
 
 int main()
 {
+    // setup_wifi();
+    // char hostname[14];
+    // sprintf(hostname+10,"hyperion-$x",Ethernet::getMac().octets[0],Ethernet::getMac().octets[1]);
+    // Network::setHostName(hostname);
+    // Log::info("","Hostname: %s",hostname);
+    
     auto hyp = new Hyperion();
 
         Tempo::AddSource(new ConstantTempo(120));
@@ -54,12 +67,36 @@ int main()
     3 * 60 * 1000);
 
 #if ESP_PLATFORM
+    // ledster big
+    // hyp->addPipe(new ConvertPipe<RGBA, GRB>(
+    //     input,
+    //     new NeopixelOutput(1),
+    //     //new SpiOutput(0,1),
+    //     ledsterLut));
+    // hyp->start(minimal);
+
+    //ledster mini
+    int size1 = 271 * sizeof(RGBA);
+    int size2 = 210 * sizeof(RGBA);
+    auto splitInput = new InputSplitter(input, {size1, size2}, true);
+
     hyp->addPipe(new ConvertPipe<RGBA, GRB>(
-        input,
-        new NeopixelOutput(1),
-        //new SpiOutput(0,1),
+        splitInput->getInput(0),
+        new NeopixelOutput(6),
         ledsterLut));
-    hyp->start(minimal);
+    hyp->addPipe(new ConvertPipe<RGBA, GRB>(
+        splitInput->getInput(1),
+        new NeopixelOutput(8),
+        ledsterLut));
+
+  hyp->start({
+      .network = false,
+      .rotary = false,
+      .display = false,
+      .midi = false,
+      .tempo = false,
+      .web = false,
+  });
 #else
     hyp->addPipe(new ConvertPipe<RGBA, RGB>(
         input,
