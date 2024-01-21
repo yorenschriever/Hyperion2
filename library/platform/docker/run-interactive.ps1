31400b1a63d6fbd90ae55b08f3aa18497e0a9747
Yoren Schriever <yoren@ocelot.nl>
2024-01-21 18:04:51 +0100

    run-interactive in pwd

--- library/platform/docker/run-interactive.ps1
+++ library/platform/docker/run-interactive.ps1
@@ -1,6 +1,6 @@
 docker build -t hyperion $env:HYPERION_LIB_DIR\platform\docker
 docker run -it --name hyperion --rm `
     --mount type=bind,source=$env:HYPERION_LIB_DIR,target=/hyperion_lib `
-    --mount type=bind,source=$projectDir,target=/project `
+    --mount type=bind,source=$pwd,target=/project `
     --workdir /project hyperion `
     /bin/bash
