4193eff8ca313b3a757187b43c171df5555bd3d4
Yoren Schriever <yoren@ocelot.nl>
2024-11-02 16:07:26 +0100

    fix build.sh

--- library/scripts/build.sh
+++ library/scripts/build.sh
@@ -22,7 +22,7 @@ if [ $TARGET = 'docker' ]; then
     docker run -it --name=hyperion --rm \
         --mount type=bind,source=${HYPERION_LIB_DIR},target=/hyperion_lib \
         --mount type=bind,source=$PWD,target=/project \
-        --mount type=bind,source=$projectDir/../common,target=/common \
+        --mount type=bind,source=$PWD/../common,target=/common \
         --workdir=/project hyperion \
         /hyperion_lib/scripts/build.sh linux
     exit;
