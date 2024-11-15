7cc680d4e383ffe731d1c1cf5ed8850eec54d31f
Yoren Schriever <yoren@ocelot.nl>
2024-11-15 15:11:05 +0100

    use canvas for cocktailbar window monitor

--- projects/cocktailbar/web/monitorWindows/monitorWindows.js
+++ projects/cocktailbar/web/monitorWindows/monitorWindows.js
@@ -1,32 +1,39 @@
-import { html } from '/common/preact-standalone.js';
+import { html, useRef } from '/common/preact-standalone.js';
 import { useSocket } from '/common/socket.js'
 
 export const MonitorWindowsApp = () => {
+    const canvasRefs = [useRef(null),useRef(null),useRef(null),useRef(null)];
+
     const handleData = (data) => {
         if (!(data instanceof ArrayBuffer))
             return;
 
         const data2 = new Uint8Array(data)
 
-        // console.log(data2);
-
         const dataToPct = (d, i) =>(d[i+1]*256 + d[i])*100/(256*256-1);
-        // const dataToPct = (d, i) =>(d[i+1])*100/0x255;
-
         for(let i=0;i<4;i++){
-            document.getElementById(`shutterTop${i}`   ).style.height = dataToPct(data2,4*i) + '%';
-            document.getElementById(`shutterBottom${i}`).style.height = dataToPct(data2,4*i+2) + '%';
+            const canvas = canvasRefs[i].current;
+            const ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = '#000';
+            
+            ctx.beginPath();
+            ctx.rect(0, 0, canvas.width, dataToPct(data2,4*i));
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.rect(0, canvas.height-dataToPct(data2,4*i+2), canvas.width, dataToPct(data2,4*i+2));
+            ctx.fill();
+
         }
     };
 
     const socket = useSocket("/ws/windows", handleData);
 
-    return html`<div class="windows"><${Window} id=0/><${Window} id=1/><${Window} id=2/><${Window} id=3/></div>`
-}
-
-export const Window = ({id}) => {
-    return html`<div class="window">
-        <div class="shutterTop" id="shutterTop${id}"></div>
-        <div class="shutterBottom" id="shutterBottom${id}"></div>
-    </div>`
+    return html`<div class="windows">${
+        canvasRefs.map(i => html`<div class="window">
+            <canvas ref=${i}  width="100" height="100"></canvas>
+        </div>`)
+    }</div>`
 }
