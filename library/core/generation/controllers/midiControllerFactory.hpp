46f0a9d021df9f988bc626d7f3bcb935abe52e2a
Jonathan van Woudenberg <jvanwoudenberg@gmail.com>
2025-03-02 18:33:43 +0100

    Fixed midi control on windows brave not working due to difference in sent names for devices.

--- library/core/generation/controllers/midiControllerFactory.hpp
+++ library/core/generation/controllers/midiControllerFactory.hpp
@@ -13,13 +13,13 @@ class MidiControllerFactory
 public:
     virtual std::unique_ptr<MidiController> create(MidiDevice *device, std::string name, ControlHub *hub)
     {
-        if (name.compare("APC MINI")==0)
+        if (name.find("APC MINI")==0)   // Use find instead of compare since some browsers add an index to the name (Brave on windows)
             return std::unique_ptr<ApcMiniController>(new ApcMiniController(hub, device, ApcMiniController::MK1));
-        if (name.compare("APC mini mk2 Control")==0)
+        if (name.find("APC mini mk2 Control")==0)
             return std::unique_ptr<ApcMiniController>(new ApcMiniController(hub, device, ApcMiniController::MK2));
-        if (name.compare("MIDI Mix")==0)
+        if (name.find("MIDI Mix")==0)
             return std::unique_ptr<MidiMixController>(new MidiMixController(hub,device));
-        if (name.compare("nanoKONTROL2 SLIDER/KNOB")==0)
+        if (name.find("nanoKONTROL2 SLIDER/KNOB")==0)
             return std::unique_ptr<KorgNanoKontrol2controller>(new KorgNanoKontrol2controller(hub,device));
         return nullptr;
     }
