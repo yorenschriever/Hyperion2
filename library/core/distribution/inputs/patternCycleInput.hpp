46222cf4a2b69c25c484661029808c22d99df5be
Yoren Schriever <yoren@ocelot.nl>
2025-12-19 16:38:34 +0100

    patterncycleinput as baseInput

--- library/core/distribution/inputs/patternCycleInput.hpp
+++ library/core/distribution/inputs/patternCycleInput.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "core/generation/patterns/pattern.hpp"
-#include "input.hpp"
+#include "baseInput.hpp"
 #include "log.hpp"
 #include "utils.hpp"
 #include <algorithm>
 
 //PatternCycleInput shows each pattern for a while.
 template <class T_COLOUR>
-class PatternCycleInput final: public Input
+class PatternCycleInput final: public BaseInput
 {
 
 public:
@@ -21,48 +21,43 @@ public:
     {
         this->length = length;
         this->patterns = patterns;
-        this->leddata = (T_COLOUR *)malloc(length * sizeof(T_COLOUR));
         this->duration = duration;
-
-        if (!this->leddata)
-        {
-            Log::error("PATTERN_INPUT", "Unable to allocate memory for PatternCycleInput, free heap = %d\n", Utils::get_free_heap());
-            Utils::exit();
-        }
     }
 
-    virtual void begin()
+    virtual void begin() override
     {
-        for (int i = 0; i < length; i++)
-            leddata[i] = T_COLOUR();
-
         for (auto pattern:patterns)
             pattern->Initialize();
 
         start = Utils::millis();
     }
 
-    virtual int loadData(uint8_t *dataPtr, unsigned int buffersize)
+    Buffer *getData() override 
     {
+        const int bufferSize = length * sizeof(T_COLOUR);
+        auto patternBuffer = BufferPool::getBuffer(bufferSize);
+        if (!patternBuffer)
+        {
+            Log::error("PATTERN_CYCLE_INPUT", "Unable to allocate memory for PatternCycleInput, free heap = %d\n", Utils::get_free_heap());
+            Utils::exit();
+        }
+        auto dataPtr = patternBuffer->getData();
+
         for (int i = 0; i < length; i++)
-            leddata[i] = T_COLOUR();
+            dataPtr[i] = T_COLOUR();
 
         for (int i=0; i < patterns.size(); i++) {
             bool active = ((Utils::millis() - start) / duration) % patterns.size() == i;
-            //active |= ((Utils::millis() - start) / duration) % patterns.size() == (i+1) % patterns.size();
-            patterns[i]->Calculate(leddata, length, active, &params);
+            patterns[i]->Calculate(dataPtr, length, active, &params);
         }
 
-        memcpy(dataPtr, leddata, std::min((int)buffersize, (int) (length * sizeof(T_COLOUR))));
+        fpsCounter.increaseUsedFrameCount();
 
-        usedframecount++;
-
-        return length * sizeof(T_COLOUR);
+        return patternBuffer;
     }
 
 private:
     int length = 0;
-    T_COLOUR *leddata;
     std::vector<Pattern<T_COLOUR>*> patterns;
     unsigned long start;
     unsigned int duration;
