9c5d1634611ceda2f46ffbfda9e556c22cd025e4
Yoren Schriever <yoren@ocelot.nl>
2026-02-06 14:55:49 +0100

    docker windows script

--- library/scripts/hyper.ps1
+++ library/scripts/hyper.ps1
@@ -51,6 +51,8 @@ if ($COMMANDS.Count -eq 0) {
 
 if ($TARGET -eq "docker")  
 {  
+    $projectDir = $($PWD.Path)
+    docker build -t hyperion $env:HYPERION_LIB_DIR\platform\docker
     docker run -it --name=hyperion --rm `
         --mount type=bind,source=$env:HYPERION_LIB_DIR,target=/hyperion_lib `
         --mount type=bind,source=$projectDir,target=/project `
